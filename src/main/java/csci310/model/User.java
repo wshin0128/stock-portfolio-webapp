3b899a99a381dcc4b0087d71df67028e5368035f
Woody <woodyshi@usc.edu>
2020-09-22 20:32:09 -0500

    Created shell method for User class constructor and test method in UserTest

--- src/main/java/csci310/model/User.java
+++ src/main/java/csci310/model/User.java
@@ -1,5 +1,36 @@
 package csci310.model;
 
 public class User {
-
+	private String username;
+	private String password;
+	private Portfolio portfolio;
+	
+	public User(String username, String password) {
+		
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public Portfolio getPortfolio() {
+		return portfolio;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public void setPortfolio(Portfolio portfolio) {
+		this.portfolio = portfolio;
+	}
+	
 }
