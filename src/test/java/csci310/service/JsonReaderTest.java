b65b11f7f58a34fa394eca007b37a603843aeacb
Chunyang Mou <cmou@usc.edu>
2020-09-23 13:25:26 -0700

    Install Jackson with maven Add JsonReaderTest.java function shell and test cases

---
+++ src/test/java/csci310/service/JsonReaderTest.java
@@ -0,0 +1,38 @@
+package csci310.service;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.Test;
+
+public class JsonReaderTest {
+	
+	String validURL = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=IBM&apikey=demo";
+	
+	String inValidURL = "some dummy value";
+	
+
+	@Test
+	public void testReadJsonFromUrl() {
+		try {
+			JSONObject jsonObject = JsonReader.readJsonFromUrl(validURL);
+			assertNotNull(jsonObject.toString());
+		} catch (JSONException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			JsonReader.readJsonFromUrl(inValidURL);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			assertNotNull(e);
+		}
+	}
+
+}
