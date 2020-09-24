2b5833aca5cceb5dc8ce432030bcedfbcaef8a53
Woody <woodyshi@usc.edu>
2020-09-23 22:09:40 -0500

    Removed Portfolio mentions in User class

--- src/main/java/csci310/model/User.java
+++ src/main/java/csci310/model/User.java
@@ -3,7 +3,6 @@ package csci310.model;
 public class User {
 	private String username;
 	private String password;
-	private Portfolio portfolio;
 	
 	public User(String username, String password) {
 		this.username = username;
@@ -18,20 +17,12 @@ public class User {
 		return password;
 	}
 	
-	public Portfolio getPortfolio() {
-		return portfolio;
-	}
-	
 	public void setUsername(String username) {
 		this.username = username;
 	}
 	
 	public void setPassword(String password) {
 		this.password = password;
-	}
-	
-	public void setPortfolio(Portfolio portfolio) {
-		this.portfolio = portfolio;
 	}
 	
 }
