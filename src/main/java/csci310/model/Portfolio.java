06fa17ed0723c9af8f76a616aa531c31588c6943
Woody <woodyshi@usc.edu>
2020-09-23 23:03:51 -0500

    Wrote implementation of Portfolio class methods

--- src/main/java/csci310/model/Portfolio.java
+++ src/main/java/csci310/model/Portfolio.java
@@ -6,31 +6,31 @@ public class Portfolio {
 	private ArrayList<String> portfolio;
 	
 	public Portfolio() {
-		
+		portfolio = new ArrayList<String>();
 	}
 	
 	public ArrayList<String> getArrayList() {
-		return null;
+		return portfolio;
 	}
 	
 	public int getSize() {
-		return 0;
+		return portfolio.size();
 	}
 	
 	public void addStock(String stock) {
-		
+		portfolio.add(stock);
 	}
 	
 	public void removeStock(String stock) {
-		
+		portfolio.remove(stock);
 	}
 	
 	public void resetPortfolio() {
-		
+		portfolio.clear();
 	}
 	
 	public boolean contains(String stock) {
-		return true;
+		return portfolio.contains(stock);
 	}
 	
 }
