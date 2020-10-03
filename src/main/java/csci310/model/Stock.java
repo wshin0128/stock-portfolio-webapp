f58e95ecab74112e51ee503cca8a937de1386ac2
Woody Shin <woodyshi@usc.edu>
2020-10-03 00:37:31 -0500

    Added shell of Stock class and test methods for Stock class

---
+++ src/main/java/csci310/model/Stock.java
@@ -0,0 +1,44 @@
+package csci310.model;
+
+public class Stock {
+	private String ticker;
+	private int quantity;
+	private String buyDate;
+	private String sellDate;
+	
+	public Stock(String ticker, int quantity, String buyDate, String sellDate) {
+		
+	}
+	
+	public String getTicker() {
+		return "";
+	}
+	
+	public int getQuantity() {
+		return 0;
+	}
+	
+	public String getBuyDate() {
+		return "";
+	}
+	
+	public String getSellDate() {
+		return "";
+	}
+	
+	public void setTicker(String ticker) {
+		
+	}
+	
+	public void setQuantity(int quantity) {
+		
+	}
+	
+	public void setBuyDate(String buyDate) {
+		
+	}
+	
+	public void setSellDate(String sellDate) {
+		
+	}
+}
---
+++ src/test/java/csci310/model/StockTest.java
@@ -0,0 +1,18 @@
+package csci310.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StockTest {
+
+	@Test
+	public void testStock() {
+		Stock s = new Stock("AAPL", 21, "1999-12-28", "2020-12-28");
+		assertTrue("Stock tickers do not match", s.getTicker() == "AAPL");
+		assertTrue("Stock quantities do not match", s.getQuantity() == 21);
+		assertTrue("Stock buy dates do not match", s.getBuyDate() == "1999-12-28");
+		assertTrue("Stock sell dates do not match", s.getSellDate() == "2020-12-28");
+	}
+
+}
