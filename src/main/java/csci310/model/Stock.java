dfc4c8592b51640728b4808cab4b27cc35aa1e3c
Woody Shin <woodyshi@usc.edu>
2020-10-03 00:46:18 -0500

    Added implementation of Stock class

--- src/main/java/csci310/model/Stock.java
+++ src/main/java/csci310/model/Stock.java
@@ -9,46 +9,49 @@ public class Stock {
 	
 	// Stock constructor
 	public Stock(String ticker, int quantity, String buyDate, String sellDate) {
-		
+		this.ticker = ticker;
+		this.quantity = quantity;
+		this.buyDate = buyDate;
+		this.sellDate = sellDate;
 	}
 	
 	// ticker getter
 	public String getTicker() {
-		return "";
+		return ticker;
 	}
 	
 	// quantity getter
 	public int getQuantity() {
-		return 0;
+		return quantity;
 	}
 	
 	// buyDate getter
 	public String getBuyDate() {
-		return "";
+		return buyDate;
 	}
 	
 	// sellDate getter
 	public String getSellDate() {
-		return "";
+		return sellDate;
 	}
 	
 	// ticker setter
 	public void setTicker(String ticker) {
-		
+		this.ticker = ticker;
 	}
 	
 	// quantity setter
 	public void setQuantity(int quantity) {
-		
+		this.quantity = quantity;
 	}
 	
 	// buyDate setter
 	public void setBuyDate(String buyDate) {
-		
+		this.buyDate = buyDate;
 	}
 	
 	// sellDate setter
 	public void setSellDate(String sellDate) {
-		
+		this.sellDate = sellDate;
 	}
 }
