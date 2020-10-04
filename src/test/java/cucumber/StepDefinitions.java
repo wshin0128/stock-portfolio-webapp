e780eb6c50977dec0b8f4aa6b6a260a2a075ef47
Cheyenne Gaimau <gaima@usc.edu>
2020-10-03 23:39:59 -0400

    sign up feature cucumber tests

--- src/test/java/cucumber/StepDefinitions.java
+++ src/test/java/cucumber/StepDefinitions.java
@@ -18,26 +18,41 @@ public class StepDefinitions {
 	private static final String ROOT_URL = "http://localhost:8080/";
 
 	private final WebDriver driver = new ChromeDriver();
-
-	@Given("I am on the index page")
-	public void i_am_on_the_index_page() {
-		driver.get(ROOT_URL);
+	
+	@Given("I am on the sign up page")
+	public void i_am_on_the_sign_up_page() {
+		driver.get(ROOT_URL+"signup.jsp");
 	}
 
-	@When("I click the link {string}")
-	public void i_click_the_link(String linkText) {
-		driver.findElement(By.linkText(linkText)).click();
+	
+	@When("I enter an invalid username {string} that already exists")
+	public void i_enter_an_invalid_username_that_already_exists(String string) {
+	    
+	}
+	@When("I enter any password")
+	public void i_enter_any_password() {
+	    
+	}
+	@Then("I should see the error {string}")
+	public void i_should_see_the_error(String string) {
+	    
 	}
 
-	@Then("I should see header {string}")
-	public void i_should_see_header(String header) {
-		assertTrue(driver.findElement(By.cssSelector("h2")).getText().contains(header));
+	/*************************************************************************/
+
+	@When("I enter a valid username {string}")
+	public void i_enter_a_valid_username(String string) {
+	   
+	}
+	@When("I enter a valid password {string}")
+	public void i_enter_a_valid_password(String string) {
+	    
+	}
+	@Then("I should land on the homepage")
+	public void i_should_land_on_the_homepage() {
+	    
 	}
 	
-	@Then("I should see text {string}")
-	public void i_should_see_text(String text) {
-		assertTrue(driver.getPageSource().contains(text));
-	}
 
 	@After()
 	public void after() {
