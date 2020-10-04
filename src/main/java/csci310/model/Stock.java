c35224f55b75e96aa3dd3426dde950647258694d
Woody Shin <woodyshi@usc.edu>
2020-10-04 00:54:53 -0500

    Added name and color variables to Stock class and updated test method

--- src/main/java/csci310/model/Stock.java
+++ src/main/java/csci310/model/Stock.java
@@ -2,22 +2,35 @@ package csci310.model;
 
 public class Stock {
 	// Stock variables
+	private String name;
 	private String ticker;
+	private String color;
 	private int quantity;
 	private String buyDate;
 	private String sellDate;
 	
 	// Stock constructor
-	public Stock(String ticker, int quantity, String buyDate, String sellDate) {
+	public Stock(String name, String ticker, int quantity, String buyDate, String sellDate) {
+		this.name = name;
 		this.ticker = ticker;
 		this.quantity = quantity;
 		this.buyDate = buyDate;
 		this.sellDate = sellDate;
 	}
 	
+	// name getter
+	public String getName() {
+		return name;
+	}
+	
 	// ticker getter
 	public String getTicker() {
 		return ticker;
+	}
+	
+	// color getter
+	public String getColor() {
+		return color;
 	}
 	
 	// quantity getter
@@ -35,9 +48,19 @@ public class Stock {
 		return sellDate;
 	}
 	
+	// name setter
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 	// ticker setter
 	public void setTicker(String ticker) {
 		this.ticker = ticker;
+	}
+	
+	// color setter
+	public void setColor(String color) {
+		this.color = color;
 	}
 	
 	// quantity setter
--- src/test/java/csci310/model/StockTest.java
+++ src/test/java/csci310/model/StockTest.java
@@ -9,11 +9,11 @@ public class StockTest {
 	// Test Stock constructor
 	@Test
 	public void testStock() {
-		Stock s = new Stock("AAPL", 21, "1999-12-28", "2020-12-28");
+		Stock s = new Stock("Apple Inc", "AAPL", 21, "1999-12-28", "2020-12-28");
+		assertTrue("Stock names do not match", s.getName() == "Apple Inc");
 		assertTrue("Stock tickers do not match", s.getTicker() == "AAPL");
 		assertTrue("Stock quantities do not match", s.getQuantity() == 21);
 		assertTrue("Stock buy dates do not match", s.getBuyDate() == "1999-12-28");
 		assertTrue("Stock sell dates do not match", s.getSellDate() == "2020-12-28");
 	}
-
 }
