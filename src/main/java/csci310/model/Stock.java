bcec28cb7ca99ab9b5163530f029471116244787
Woody Shin <woodyshi@usc.edu>
2020-10-08 18:18:10 -0500

    Removed LomBok getter/setter plugin

--- src/main/java/csci310/model/Stock.java
+++ src/main/java/csci310/model/Stock.java
@@ -30,11 +30,12 @@ public class Stock {
 	public String assignHexColor() {
 		Random rand = new Random();
 		
-		// Generate random number with maximum being #FFFFFF
-        int rawColor = rand.nextInt(0xffffff + 1);
+		
+        int rawColor = 16777215;
         
         // If rawColor is white, generate a new color
         while(rawColor == 16777215) {
+        	// Generate random number with maximum being #FFFFFF
         	rawColor = rand.nextInt(0xffffff + 1);
         }
                 
@@ -46,7 +47,63 @@ public class Stock {
 		return color;
 	}
 	
+	// name getter
+	public String getName() {
+		return name;
+	}
 	
+	// ticker getter
+	public String getTicker() {
+		return ticker;
+	}
 	
+	// color getter
+	public String getColor() {
+		return color;
+	}
 	
+	// quantity getter
+	public int getQuantity() {
+		return quantity;
+	}
+	
+	// buyDate getter
+	public Integer getBuyDate() {
+		return buyDate;
+	}
+	
+	// sellDate getter
+	public Integer getSellDate() {
+		return sellDate;
+	}
+	
+	// name setter
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	// ticker setter
+	public void setTicker(String ticker) {
+		this.ticker = ticker;
+	}
+	
+	// color setter
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
+	// quantity setter
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	
+	// buyDate setter
+	public void setBuyDate(Integer buyDate) {
+		this.buyDate = buyDate;
+	}
+	
+	// sellDate setter
+	public void setSellDate(Integer sellDate) {
+		this.sellDate = sellDate;
+	}
 }
