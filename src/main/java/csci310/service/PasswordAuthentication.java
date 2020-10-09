e5d55eb6e64f21b39b20b47ace7a8b249a3434ff
Manav Kaushik <manavkau@usc.edu>
2020-10-08 21:24:37 -0400

    Comment password authentication

--- src/main/java/csci310/service/PasswordAuthentication.java
+++ src/main/java/csci310/service/PasswordAuthentication.java
@@ -4,9 +4,9 @@ import java.security.NoSuchAlgorithmException;
 import java.util.Date;
 import java.util.List;
 import java.util.regex.Pattern;
-
+//Security for password hashing
 public class PasswordAuthentication{
-	
+	//Function for MD5 hashing string, returns hashed string
 	public String MD5(String md5) throws NoSuchAlgorithmException {
 		java.security.MessageDigest md = java.security.MessageDigest.getInstance("MD5");
 		byte[] array = md.digest(md5.getBytes());
@@ -17,7 +17,9 @@ public class PasswordAuthentication{
         return sb.toString();
 	   
 	}
-	
+	//Main hash function, takes in raw password, and salt and round values for password
+	//Hashing uses MD5 and salt and rounds
+	//Returns hashed strings
 	public String hash(String rawPassword, Long salt, Integer rounds) throws NoSuchAlgorithmException {
 		salt = salt != null ? salt : new Date().getTime();
 		rounds = rounds != null ? rounds : 10;
@@ -27,7 +29,8 @@ public class PasswordAuthentication{
 		}
 		return Long.toString(salt) + "$" + Integer.toString(rounds) + "$" + hashedString;
 	}
-	
+	//Verifies that the raw password fits with the hashedpassword in the database
+	//Returns boolean
 	public boolean verify(String rawPassword, String hashedPassword) throws NoSuchAlgorithmException {
 		String[] splittedList= hashedPassword.split(Pattern.quote("$"));
 		long saltLong = Long.valueOf(splittedList[0]);
