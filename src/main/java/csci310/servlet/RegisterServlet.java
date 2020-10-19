2a5910e8c5d6892f39587a2df8ca16a03d3e127e
Manav Kaushik <manavkau@usc.edu>
2020-10-18 23:18:37 -0400

    TDD: Register Servlet

---
+++ src/main/java/csci310/servlet/RegisterServlet.java
@@ -0,0 +1,32 @@
+package csci310.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.security.NoSuchAlgorithmException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.stream.Collectors;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.json.JSONObject;
+
+import csci310.service.DatabaseClient;
+import csci310.service.PasswordAuthentication;
+
+public class RegisterServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
