b794d4aa2c57d57e3c42702758eb748bedd039c1
Woody Shin <woodyshi@usc.edu>
2020-10-21 01:14:58 -0500

    Shell of AddStockServlet.java doPost() and corresponding tests

---
+++ src/main/java/csci310/servlet/AddStockServlet.java
@@ -0,0 +1,16 @@
+package csci310.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class AddStockServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException {
+		
+	}
+}
