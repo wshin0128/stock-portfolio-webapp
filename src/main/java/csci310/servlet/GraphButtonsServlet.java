e8be6953bb346372f9f9297b2ca18cce79994bd7
Nandhakumar Saravana <nsaravan@usc.edu>
2020-10-31 22:32:32 -0700

    Added more tests and implementation

--- src/main/java/csci310/servlet/GraphButtonsServlet.java
+++ src/main/java/csci310/servlet/GraphButtonsServlet.java
@@ -28,9 +28,54 @@ public class GraphButtonsServlet extends HttpServlet{
 	
 	@Override
 	protected void doPost(final HttpServletRequest request, final HttpServletResponse response) throws IOException, ServletException {
+try {
+		HttpSession session = request.getSession();
+		System.out.println(request.getParameter("timePeriod"));
+		System.out.println(request.getParameter("SNP500"));
+		
+		String tp = (String)request.getParameter("timePeriod");
+		String snp = (String)request.getParameter("SNP500");
+		
+//		if(snp!=null)
+//		{
+//			
+//			session.setAttribute("snp", 1);
+//			request.getRequestDispatcher("/homepage.jsp").forward(request, response);
+//		}
+//		else
+//		{
+//			session.setAttribute("snp", null);
+//		}
+		
+		if(tp.equals("1D"))
+		{
+			session.setAttribute("tp", "1");
+			System.out.println("set tp to 1");
+		}
+		else if(tp.equals("1W"))
+		{
+			session.setAttribute("tp", "2");
+			System.out.println("set tp to 2");
+		}
+		else if(tp.equals("1M"))
+		{
+			session.setAttribute("tp", "3");
+			System.out.println("set tp to 3");
+		}
+		else if(tp.equals("1Y"))
+		{
+			session.setAttribute("tp", "4");
+			System.out.println("set tp to 4");
+		}
 		
 		
-		
+			request.getRequestDispatcher("/homepage.jsp").forward(request, response);	
+		}
+		catch (Exception e) {
+			System.out.println("Exception from GraphButtons.doPost()");
+			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+		}
+			
 	}
 	
 }
--- src/test/java/csci310/servlet/GraphButtonsServletTest.java
+++ src/test/java/csci310/servlet/GraphButtonsServletTest.java
@@ -3,6 +3,7 @@ package csci310.servlet;
 import static org.junit.Assert.*;
 
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -26,6 +27,94 @@ public class GraphButtonsServletTest extends Mockito {
 		RequestDispatcher rd = mock(RequestDispatcher.class);
 		
 		assertTrue(true);
+		
+	}
+	
+	@Test
+	public void testDoPost2() throws Exception {
+		
+		DatabaseClient dbc = new DatabaseClient();
+		dbc.createTable();
+
+		HttpServletRequest request = mock(HttpServletRequest.class);
+		HttpServletResponse response = mock(HttpServletResponse.class);
+		HttpSession session = mock(HttpSession.class);
+		RequestDispatcher rd = mock(RequestDispatcher.class);
+		
+		when(request.getParameter("timePeriod")).thenReturn("1D");
+		when(request.getSession()).thenReturn(session);
+		when(request.getRequestDispatcher("/homepage.jsp")).thenReturn(rd);
+		
+		GraphButtonsServlet g = new GraphButtonsServlet();
+		g.doPost(request, response);
+		
+		verify(session).setAttribute("tp", "1");
+		
+	}
+	
+	@Test
+	public void testDoPost3() throws Exception {
+		
+		DatabaseClient dbc = new DatabaseClient();
+		dbc.createTable();
+
+		HttpServletRequest request = mock(HttpServletRequest.class);
+		HttpServletResponse response = mock(HttpServletResponse.class);
+		HttpSession session = mock(HttpSession.class);
+		RequestDispatcher rd = mock(RequestDispatcher.class);
+		
+		when(request.getParameter("timePeriod")).thenReturn("1W");
+		when(request.getSession()).thenReturn(session);
+		when(request.getRequestDispatcher("/homepage.jsp")).thenReturn(rd);
+		
+		GraphButtonsServlet g = new GraphButtonsServlet();
+		g.doPost(request, response);
+		
+		verify(session).setAttribute("tp", "2");
+		
+	}
+	
+	@Test
+	public void testDoPost4() throws Exception {
+		
+		DatabaseClient dbc = new DatabaseClient();
+		dbc.createTable();
+
+		HttpServletRequest request = mock(HttpServletRequest.class);
+		HttpServletResponse response = mock(HttpServletResponse.class);
+		HttpSession session = mock(HttpSession.class);
+		RequestDispatcher rd = mock(RequestDispatcher.class);
+		
+		when(request.getParameter("timePeriod")).thenReturn("1M");
+		when(request.getSession()).thenReturn(session);
+		when(request.getRequestDispatcher("/homepage.jsp")).thenReturn(rd);
+		
+		GraphButtonsServlet g = new GraphButtonsServlet();
+		g.doPost(request, response);
+		
+		verify(session).setAttribute("tp", "3");
+		
+	}
+	
+	@Test
+	public void testDoPost5() throws Exception {
+		
+		DatabaseClient dbc = new DatabaseClient();
+		dbc.createTable();
+
+		HttpServletRequest request = mock(HttpServletRequest.class);
+		HttpServletResponse response = mock(HttpServletResponse.class);
+		HttpSession session = mock(HttpSession.class);
+		RequestDispatcher rd = mock(RequestDispatcher.class);
+		
+		when(request.getParameter("timePeriod")).thenReturn("1Y");
+		when(request.getSession()).thenReturn(session);
+		when(request.getRequestDispatcher("/homepage.jsp")).thenReturn(rd);
+		
+		GraphButtonsServlet g = new GraphButtonsServlet();
+		g.doPost(request, response);
+		
+		verify(session).setAttribute("tp", "4");
 		
 	}
 
