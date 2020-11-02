5cc222aff8a919ab30773fb8838f5d428a7ecaa1
markzcc <wzhan@usc.edu>
2020-11-02 02:25:39 -0800

    Shell for CSV servlet along with test methods
    Edited homepage.jsp for uploading file form, and web.xml for servlet
    mapping

---
+++ src/main/java/csci310/servlet/CSVServlet.java
@@ -0,0 +1,18 @@
+package csci310.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class CSVServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// shell for do post
+	}
+}
