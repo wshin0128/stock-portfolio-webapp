bcd49c3ca1f2c7508fd9f96759634155fbae162a
Woody Shin <woodyshi@usc.edu>
2020-11-04 03:14:42 -0600

    Updated ViewStockServlet and corresponding test methods

--- src/main/java/csci310/servlet/ViewStockServlet.java
+++ src/main/java/csci310/servlet/ViewStockServlet.java
@@ -19,24 +19,37 @@ public class ViewStockServlet extends HttpServlet {
 		try {		
 			DatabaseClient dbc = new DatabaseClient();
 			FinnhubClient fc = new FinnhubClient();
-
-			// Get form values
-			String ticker = request.getParameter("ticker");
-			int shares = Integer.parseInt(request.getParameter("shares"));
-			String datePurchased = request.getParameter("date-purchased");
-			String dateSold = request.getParameter("date-sold");
-						
-			// Convert dates to UNIX time
-			SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd");
-			Date datePurchasedObject = dateFormatter.parse(datePurchased);
-			long datePurchasedUnix = datePurchasedObject.getTime();
-			Date dateSoldObject = dateFormatter.parse(dateSold);
-			long dateSoldUnix = dateSoldObject.getTime();
 			
-			// Get userID from session
-			int userID = (Integer) request.getSession().getAttribute("userID");
+			boolean formError = false;
+			
+			// Check if form was populated
+			if(request.getParameter("ticker").equals("")) {
+				request.setAttribute("viewedErrorMessageTicker", "Illegal ticker symbol");
+				formError = true;
+			}
+			if(request.getParameter("shares").equalsIgnoreCase("")) {
+				request.setAttribute("viewedErrorMessageShares", "Negative or zero values of quantity");
+				formError = true;
+			}
+			if((!request.getParameter("date-sold").equals("")) && (request.getParameter("date-purchased").equals(""))) {
+				request.setAttribute("viewedErrorMessageDateSold", "Sold date with no purchase date");
+				formError = true;
+			}
+			if((request.getParameter("date-sold").equals("")) && (!request.getParameter("date-purchased").equals(""))) {
+				request.setAttribute("viewedErrorMessageDatePurchased", "Purchase date with no sold date");
+				formError = true;
+			}
+			if(request.getParameter("date-purchased").equals("")) {
+				request.setAttribute("viewedErrorMessageDatePurchased", "Purchase date is required");
+				formError = true;
+			}
+			if(request.getParameter("date-sold").equals("")) {
+				request.setAttribute("viewedErrorMessageDateSold", "Sold date is required");
+				formError = true;
+			}
 			
 			// Lookup stock name using ticker
+			String ticker = request.getParameter("ticker").toUpperCase();
 			String companyName = "";
 			boolean validTicker = true;
 			try {
@@ -47,48 +60,83 @@ public class ViewStockServlet extends HttpServlet {
 			
 			// If stock ticker is invalid
 			if(!validTicker) {
-				System.out.println("Ticker invalid");
-				request.setAttribute("viewStockErrorMessage", "Your ticker is invalid");
-				request.getRequestDispatcher("/homepage.jsp").forward(request, response);
-				return;
+				request.setAttribute("viewedErrorMessageTicker", "Illegal ticker symbol");
+				formError = true;
 			}
+			
 			// If number of shares is 0 or smaller, set error
-			else if(shares <= 0) {
-				System.out.println("Shares invalid");
-				request.setAttribute("viewStockErrorMessage", "You must buy at least one share");
-				request.getRequestDispatcher("/homepage.jsp").forward(request, response);
-				return;
-			}
-			// If purchase date is after sold date, set error and go back home
-			else if(datePurchasedUnix >= dateSoldUnix) {
-				System.out.println("Dates invalid");
-				request.setAttribute("viewStockErrorMessage", "Your sell date must be after your buy date");
-				request.getRequestDispatcher("/homepage.jsp").forward(request, response);
-				return;
-			}
-			// No errors, add stock to database
-			else {
-				// Color override (Admin feature, not visible to user)
-				HomePageModule homePageModule = (HomePageModule) request.getSession().getAttribute("module");
-				if(request.getAttribute("colorOverride") != null) {
-					Stock s = new Stock(companyName, ticker, (String)request.getAttribute("colorOverride"), shares, datePurchasedUnix, dateSoldUnix);
-					homePageModule.addViewedStock(s);
-					dbc.addStockToViewed(userID, s);
-				} else {
-					Stock s = new Stock(companyName, ticker, null, shares, datePurchasedUnix, dateSoldUnix);
-					homePageModule.addViewedStock(s);
-					dbc.addStockToViewed(userID, s);
+			int shares = 0;
+			if(!request.getParameter("shares").equalsIgnoreCase("")) {
+				shares = Integer.parseInt(request.getParameter("shares"));
+				if(shares <= 0) {
+					request.setAttribute("viewedErrorMessageShares", "Negative or zero values of quantity");
+					formError = true;
 				}
+			}
+
+			// Check dates
+			String datePurchased = "";
+			String dateSold = "";
+			long datePurchasedUnix = 0;
+			long dateSoldUnix = 0;
+			if(!request.getParameter("date-purchased").equalsIgnoreCase("") && !request.getParameter("date-sold").equalsIgnoreCase("")) {
+				// Get form values
+				datePurchased = request.getParameter("date-purchased");
+				dateSold = request.getParameter("date-sold");
+
+				// Convert dates to UNIX time
+				SimpleDateFormat dateFormatter = new SimpleDateFormat("MM/dd/yyy");
+				Date datePurchasedObject = dateFormatter.parse(datePurchased);
+				datePurchasedUnix = datePurchasedObject.getTime();
+				Date dateSoldObject = dateFormatter.parse(dateSold);
+				dateSoldUnix = dateSoldObject.getTime();
+				
+				// Validate date
+				Date now = new Date();
+		        long currentTimeUnix = now.getTime() / 1000L;
+		        long oneYearAgo = (currentTimeUnix - 31536000) * 1000;
+		        
+		        // If purchase date is after sold date, set error and go back home
+		        if(datePurchasedUnix >= dateSoldUnix) {
+		        	request.setAttribute("viewedErrorMessageDateSold", "Sold date prior to purchase date");
+		        	formError = true;
+				}
+		        
+		        // Make sure no dates are older than one year ago
+		        if(datePurchasedUnix < oneYearAgo) {
+		        	request.setAttribute("viewedErrorMessageDatePurchased", "Purchase date cannot be older than 1 year ago");
+		        	formError = true;
+		        }
+		        if(dateSoldUnix < oneYearAgo) {
+		        	request.setAttribute("viewedErrorMessageDateSold", "Sold date cannot be older than 1 year ago");
+		        	formError = true;
+		        }
+			}
+			
+			if(formError) {
+				request.getRequestDispatcher("/homepage.jsp").forward(request, response);
+				return;
+			}
+			
+			// Get userID from session
+			int userID = (Integer) request.getSession().getAttribute("userID");
+			
+			// Color override (Admin feature, not visible to user)
+			HomePageModule homePageModule = (HomePageModule) request.getSession().getAttribute("module");
+			if(request.getAttribute("colorOverride") != null) {
+				Stock s = new Stock(companyName, ticker, (String)request.getAttribute("colorOverride"), shares, datePurchasedUnix, dateSoldUnix);
+				homePageModule.addViewedStock(s); }
+			else {
+				Stock s = new Stock(companyName, ticker, null, shares, datePurchasedUnix, dateSoldUnix);
+				homePageModule.addViewedStock(s);
 			}
 			
 			// Go back to homepage
-			System.out.println("No errors");
 			request.getRequestDispatcher("/homepage.jsp").forward(request, response);
 			return;
 			
 		} catch (Exception e) {
 			System.out.println("Exception from ViewStockServlet.doPost()");
-			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-		}
+			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);}
 	}
 }
